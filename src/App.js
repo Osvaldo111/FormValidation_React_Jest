6fe714c896e2a1ca478a22337881f1dbd9613f75
Osvaldo <osvaldofabricio11@gmail.com>
2020-03-19 23:55:34 -0500

    Form completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, {Component} from 'react';
-import Form from './components/Form'
-import Message from './components/Message'
+import React, { Component } from "react";
+import Form from "./components/Form";
+import Message from "./components/Message";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+  }
 
-    render() {
-        return (<div>
-            <Form></Form>
-            <Message></Message>
-        </div>);
-    }
+  messageDisplay = message => {
+    this.setState({ message: message });
+  };
+  render() {
+    return (
+      <div>
+        <Form messageFunc={this.messageDisplay}></Form>
+        <Message message={this.state.message}></Message>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,155 @@
-import React, {Component} from 'react';
-import {PropTypes} from 'prop-types';
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEmailValid: false,
-            isNameValid: false,
-            isPhoneValid: false,
-            isUrlValid: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEmailValid: false,
+      isNameValid: false,
+      isPhoneValid: false,
+      isUrlValid: false,
+      name: "",
+      email: "",
+      phone: "",
+      blogURL: "",
+      message: ""
+    };
+  }
 
+  handleChangeName = event => {
+    this.setState({ name: event.target.value });
+  };
+  handleChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  };
+  handleChangePhone = event => {
+    this.setState({ phone: event.target.value });
+  };
+  handleChangeBlogURL = event => {
+    this.setState({ blogURL: event.target.value });
+  };
+
+  submitForm = () => {
+    Promise.resolve(1)
+      .then(() => {
+        this.verifyName();
+        this.verifyEmail();
+        this.verifyPhone();
+        this.verifyURL();
+      })
+      .then(() => {
+        const {
+          isNameValid,
+          isEmailValid,
+          isPhoneValid,
+          isUrlValid
+        } = this.state;
+        if (isNameValid && isEmailValid && isPhoneValid && isUrlValid) {
+          this.props.messageFunc("Form is Complete!");
+        } else {
+          this.props.messageFunc("Form is Incomplete!");
+        }
+
+        console.log(this.state);
+      });
+  };
+
+  verifyName = () => {
+    const { name } = this.state;
+    var letters = /^[A-Za-z]+$/;
+    const lengthName = name.length;
+    if (lengthName >= 3 && lengthName <= 30 && name.match(letters)) {
+      this.setState({ isNameValid: true });
+    } else {
+      this.setState({ isNameValid: false });
     }
-    
-    render() {
-        return (
-            <div className="row">
-            <h1 className="text-center">Form Validation</h1>
-            <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
-                <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
-                </div>
-            </form>
-        </div>);
+  };
+
+  verifyEmail = () => {
+    const { email } = this.state;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (email.match(emailPattern)) {
+      this.setState({ isEmailValid: true });
+    } else {
+      this.setState({ isEmailValid: false });
     }
+  };
+
+  verifyPhone = () => {
+    const { phone } = this.state;
+    const lengthPhone = phone.length;
+    const firstNum = phone.charAt(0);
+    const lastNum = phone.charAt(lengthPhone - 1);
+    if (
+      lengthPhone === 10 &&
+      firstNum != 0 &&
+      firstNum !== 1 &&
+      lastNum != 0 &&
+      lastNum != 1
+    ) {
+      this.setState({ isPhoneValid: true });
+    } else {
+      this.setState({ isPhoneValid: false });
+    }
+  };
+
+  verifyURL = () => {
+    const { blogURL } = this.state;
+    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    if (blogURL.match(regex)) {
+      this.setState({ isUrlValid: true });
+    } else {
+      this.setState({ isUrlValid: false });
+    }
+  };
+  render() {
+    const { name, email, phone, blogURL, message } = this.state;
+    return (
+      <div className="row">
+        <h1 className="text-center">Form Validation</h1>
+        <form>
+          <h3>
+            Name:
+            <input type="text" value={name} onChange={this.handleChangeName} />
+          </h3>
+          <h3>
+            Email:
+            <input
+              type="text"
+              value={email}
+              onChange={this.handleChangeEmail}
+            />
+          </h3>
+          <h3>
+            Phone:
+            <input
+              type="text"
+              value={phone}
+              onChange={this.handleChangePhone}
+            />
+          </h3>
+          <h3>
+            Blog URL:
+            <input
+              type="text"
+              value={blogURL}
+              onChange={this.handleChangeBlogURL}
+            />
+          </h3>
+          <div className="small-6 small-centered text-center columns">
+            <a
+              href="#"
+              className="button success expand round text-center"
+              onClick={this.submitForm}
+            >
+              Verify
+            </a>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
